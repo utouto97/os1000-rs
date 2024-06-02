18e22cbf160981d3847273b15a402da807dc3e53
utouto97 <utouto97@gmail.com>
2024-06-02 11:02:24 +0900

    例外処理

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -35,3 +35,60 @@ macro_rules! println {
         $crate::print!("{}\n", format_args!($($arg)*));
     };
 }
+
+#[repr(C, packed)]
+pub struct TrapFrame {
+    pub ra: u32,
+    pub gp: u32,
+    pub tp: u32,
+    pub t0: u32,
+    pub t1: u32,
+    pub t2: u32,
+    pub t3: u32,
+    pub t4: u32,
+    pub t5: u32,
+    pub t6: u32,
+    pub a0: u32,
+    pub a1: u32,
+    pub a2: u32,
+    pub a3: u32,
+    pub a4: u32,
+    pub a5: u32,
+    pub a6: u32,
+    pub a7: u32,
+    pub s0: u32,
+    pub s1: u32,
+    pub s2: u32,
+    pub s3: u32,
+    pub s4: u32,
+    pub s5: u32,
+    pub s6: u32,
+    pub s7: u32,
+    pub s8: u32,
+    pub s9: u32,
+    pub s10: u32,
+    pub s11: u32,
+    pub sp: u32,
+}
+
+#[macro_export]
+macro_rules! read_csr {
+    ($csr:expr) => {
+        unsafe {
+            use core::arch::asm;
+            let mut csrr: u32;
+            asm!(concat!("csrr {r}, ", $csr), r = out(reg) csrr);
+            csrr
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! write_csr {
+    ($csr:expr, $value:expr) => {
+        unsafe {
+            use core::arch::asm;
+            asm!(concat!("csrw ", $csr, ", {r}"), r = in(reg) $value);
+        }
+    };
+}
